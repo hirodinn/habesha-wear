b9845a04638c91660adfc2e6bf93c7df8116890e
hirodinn <hirebikila1@gmail.com>
2026-01-02 17:44:14 +0300

    feat: enhance user authentication flow with token-based access and improved error handling

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,15 +1,20 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import { User, validateNewUser, validateUserLogin } from "../model/user.js";
-import { validateId } from "../utils/validateId.js";
 
 const router = express.Router();
 
-router.get("/:id", async (req, res) => {
-  const { error } = validateId(req.params.id);
-  if (error) return res.status(400).send("Invalid user ID");
+router.get("/me", async (req, res) => {
+  res.set({
+    "Cache-Control": "no-store",
+    Pragma: "no-cache",
+    Expires: "0",
+  });
+  const token = req.cookies.token;
+  if (!token)
+    return res.status(401).json({ success: false, message: "Access denied" });
   try {
-    const user = await User.findById(req.params.id).select("-password");
+    const user = await User.findByToken(token).select("-password");
     if (!user)
       return res
         .status(404)
@@ -32,6 +37,13 @@ router.post("/", async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
     user.password = hashedPassword;
     user = await user.save();
+    res.cookie("token", user.getAuthToken(), {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      path: "/",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
     res.status(200).send(user);
   } catch (err) {
     res.status(400).json({ success: false, message: err.message });
@@ -46,11 +58,28 @@ router.post("/login", async (req, res) => {
       .json({ success: false, message: error.details[0].message });
   try {
     const user = await User.findOne({ email: req.body.email });
-    if (!user || !(await user.comparePassword(req.body.password))) {
+    if (!user)
       return res
-        .status(400)
-        .json({ success: false, message: "Invalid email or password" });
-    }
+        .status(404)
+        .json({ success: false, message: "email or password error" });
+    const isValid = await bcrypt.compare(req.body.password, user.password);
+    const token = user.getAuthToken();
+    if (isValid) {
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        path: "/",
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+      res.json({
+        success: true,
+        message: `Login successful, Welcome ${user.name}`,
+      });
+    } else
+      res
+        .status(404)
+        .json({ success: false, message: "email or password error" });
     res.send(user);
   } catch (err) {
     res.status(500).json({ success: false, message: "Server error" });
